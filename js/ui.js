76033afa9e133864d66e4d2b81fa78ae3e942ffe
PROTIK DAS UTSO <130892078+protik-das-utso@users.noreply.github.com>
2024-04-14 19:29:04 +0600

    Update color

--- js/ui.js
+++ js/ui.js
@@ -4,10 +4,19 @@ class UI {
     }
 
     showProfile(user) {
+        if (user.company == null) {
+            user.company = "Not Given";
+        }
+        if (user.bio == null) {
+            user.bio = "Not Given";
+        }
+        if (user.location == null) {
+            user.location = "Not Given";
+        }
         this.profile.innerHTML = `
             <div class="profile border p-4 flex flex-col md:flex-row">
                 <div class="mb-4 md:mr-4">
-                <p class="bg-purple-200 p-3 rounded-full hover:bg-purple-400 text-center hover:text-white mb-2"> Profile Picture </p>
+                <p class="bg-green-500 p-3 rounded-full hover:bg-green-400 text-center hover:text-white mb-2"> Profile Picture </p>
                 <img src="${user.avatar_url}"
                     class="object-cover w-full mt-9 rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
                     alt=" avatar" srcset="">
@@ -15,21 +24,21 @@ class UI {
             <div class="document flex-1">
                 <div class="btn-top mb-4">
                     <button type="button"
-                        class="bg-purple-200 p-3 rounded-full text-sm hover:bg-purple-400 hover:text-white"><a href= "${user.html_url}" target="_blank">Visit Profile</a>
+                        class="bg-green-500 p-3 rounded-full text-sm hover:bg-green-400 hover:text-white"><a href= "${user.html_url}" target="_blank">Visit Profile</a>
                         <span>&#8599;</span></button>
                     <button type="button"
-                        class="bg-purple-200 p-3 rounded-full text-sm hover:bg-purple-400 hover:text-white mb-2">Follower <span class="bg-purple-300 rounded-full p-2">${user.followers}</span></button></button>
+                        class="bg-green-500 p-3 rounded-full text-sm hover:bg-green-400 mb-2">Follower <span class="bg-white rounded-full p-2">${user.followers}</span></button></button>
                     <button type="button"
-                        class="bg-purple-200 p-3 rounded-full text-sm hover:bg-purple-400 hover:text-white mb-2">Following <span class="bg-purple-300 rounded-full p-2">${user.following}</span></button>
+                        class="bg-green-500 p-3 rounded-full text-sm hover:bg-green-400 mb-2">Following <span class="bg-white rounded-full p-2">${user.following}</span></button>
                     <button type="button"
-                        class="bg-purple-200 p-3 rounded-full text-sm hover:bg-purple-400 hover:text-white mb-2"><a href="${user.blog}">Blog</a><span>&#8599;</span></button>
+                        class="bg-green-500 p-3 rounded-full text-sm hover:bg-green-400 mb-2 hover:text-white"><a href="${user.blog}">Blog</a><span>&#8599;</span></button>
                     <button type="button"
-                        class="bg-purple-200 p-3 rounded-full text-sm hover:bg-purple-400 hover:text-white mb-2"><a href="https://github.com/${user.login}?tab=repositories" target="_blank">Public
-                        Repository </a><span class="bg-purple-300 rounded-full p-2">${user.public_repos}</span><span>&#8599;</span></button>
+                        class="bg-green-500 p-3 rounded-full text-sm hover:bg-green-400 mb-2"><a href="https://github.com/${user.login}?tab=repositories" target="_blank">Public
+                        Repository </a><span class="bg-white rounded-full p-2">${user.public_repos}</span><span>&#8599;</span></button>
 
                     <button type="button"
-                        class="bg-purple-200 p-3 rounded-full text-sm hover:bg-purple-400 hover:text-white">Public
-                        Gists <span class="bg-purple-300 rounded-full p-2">${user.public_gists}</span></button>
+                        class="bg-green-500 p-3 rounded-full text-sm hover:bg-green-400">Public
+                        Gists <span class="bg-white rounded-full p-2">${user.public_gists}</span></button>
 
                 </div>
                 <div class="description pr-3">
